02bf3dc2d636411ab488a0af0d4cc1cfc225b757
Devi Sri Varaprasad Matcha <dspnaani143@gmail.com>
2026-06-15 16:06:39 +0530

    List and list Methods

---
+++ .ipynb_checkpoints/Logical operators-checkpoint.py
@@ -0,0 +1,24 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Logical operators
+>>> a , b = 5 ,4
+>>> a > b and b<a
+True
+>>> print(a<b and a>b)
+False
+>>> print(a==b or a>=b)
+True
+>>> print(a<b or a>b)
+True
+>>> print(10 and 5)
+5
+>>> 0 and 5
+0
+>>> 0 and 0
+0
+>>> 10 or 0
+10
+>>> 10 or 5
+10
+>>> 0 or 0
+0
---
+++ .ipynb_checkpoints/Relation operators-checkpoint.py
@@ -0,0 +1,18 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Relation Operators
+>>> 10>5
+True
+>>> a,b = 10 , 11
+>>> print(a>5)
+True
+>>> print(a<b)
+True
+>>> print(a<=b)
+True
+>>> print(a>=b)
+False
+>>> print(a==b)
+False
+>>> print(a!=b)
+True
